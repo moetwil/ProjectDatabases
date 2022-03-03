f0325be0cfc7b83137190e42e4362869353d49a2
moetwil <luc.moetwil@gmail.com>
2022-03-03 10:00:57 +0100

    Someren-master

---
+++ Someren-master/SomerenModel/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Student
+    { 
+        public string Name { get; set; }
+        public int Number { get; set; } // StudentNumber, e.g. 474791
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ Someren-master/SomerenUI/SomerenUI.cs
@@ -0,0 +1,100 @@
+﻿using SomerenLogic;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomerenUI
+{
+    public partial class SomerenUI : Form
+    {
+        public SomerenUI()
+        {
+            InitializeComponent();
+        }
+
+        private void SomerenUI_Load(object sender, EventArgs e)
+        {
+            showPanel("Dashboard");
+        }
+
+        private void showPanel(string panelName)
+        {
+
+            if (panelName == "Dashboard")
+            {
+                // hide all other panels
+                pnlStudents.Hide();
+
+                // show dashboard
+                pnlDashboard.Show();
+                imgDashboard.Show();
+            }
+            else if (panelName == "Students")
+            {
+                // hide all other panels
+                pnlDashboard.Hide();
+                imgDashboard.Hide();
+
+                // show students
+                pnlStudents.Show();
+
+                try
+                {
+                    // fill the students listview within the students panel with a list of students
+                    StudentService studService = new StudentService(); ;
+                    List<Student> studentList = studService.GetStudents(); ;
+
+                    // clear the listview before filling it again
+                    listViewStudents.Clear();
+
+                    foreach (Student s in studentList)
+                    {
+                        ListViewItem li = new ListViewItem(s.Name);
+                        listViewStudents.Items.Add(li);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Something went wrong while loading the students: " + e.Message);
+                }
+            }
+        }
+
+        private void dashboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dashboardToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            showPanel("Dashboard");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgDashboard_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("What happens in Someren, stays in Someren!");
+        }
+
+        private void studentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showPanel("Students");
+        }
+    }
+}
