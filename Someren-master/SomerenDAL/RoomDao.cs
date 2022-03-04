9d90a800fccaff6b567f66f17c6db301b2d6f36e
moetwil <luc.moetwil@gmail.com>
2022-03-04 14:23:29 +0100

    RoomDao

---
+++ Someren-master/SomerenDAL/RoomDao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class RoomDao : BaseDao
+    {      
+        public List<Room> GetAllRooms()
+        {
+            string query = "SELECT roomId, capacity, roomType FROM [Rooms]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Room> ReadTables(DataTable dataTable)
+        {
+            List<Room> students = new List<Room>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Room room = new Room()
+                {
+                    Number = (int)dr["roomId"],
+                    Capacity = (int)dr["capacity"],
+                    Type = (bool)dr["roomType"]
+                    
+                };
+                students.Add(room);
+            }
+            return students;
+        }
+    }
+}
