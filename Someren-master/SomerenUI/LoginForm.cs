352819a98ea7f4def3072df8b0700649ee5fe8b4
moetwil <luc.moetwil@gmail.com>
2022-03-22 19:18:49 +0100

    check lege velden + formulier gestyled

--- Someren-master/SomerenUI/LoginForm.cs
+++ Someren-master/SomerenUI/LoginForm.cs
@@ -15,7 +15,6 @@ namespace SomerenUI
         private void loginButton_Click(object sender, EventArgs e)
         {
             // for testing
-
             /*string hash = "LUKjEWABxKwpc3R4u27CLi4ZoUlbdRVDdu0GnGoowaK3KL/TxKUUwigih9DBP/B2lc+9ZxOG253IhSmpZ/TryUqSW0vGFuRYcWSf3qIFoBDqSubkZv3Qe+mW2fi8cp3yCFEpjtYT+wyObPWsWzL1LalERyg6eQcmU7dahkpYAIDXYjMs27nawM37h7OP5Cg11AKI3J/RKJzGPEZ67r9sCHRJHPASlTL1LFEJTEUZxbjfSprq6y/Ve5wXJUFBbFOBicnRPXmbBJgsC4Ij1hA5N+MFTwmmpq/lEdtFfKtFjvRt7KYXVJVSp3rw78mnIl+Bc6OhvJE9P4YG/JDyPBdnKQ==";
             string salt = "pkBQcnWtztlIArwFEHtkDUTjfEqpePLu0zJLo7hpRXu7wV84AYwU6FJG00OHMnuvkG65HyRJYkH887pNzRqDlQ==";
             HashSalt hashSalt = new HashSalt(hash, salt);
@@ -25,17 +24,42 @@ namespace SomerenUI
 
             MessageBox.Show(login.ToString());*/
 
+
+
             // save the textbox values
+
             string username = usernameTextbox.Text;
             string password = passwordTextBox.Text;
 
-            // get an user by the given username
-            UserService userService = new UserService();
-            User user = userService.GetUser(username);
+            
 
-            // verify the login
-            HashSaltService hashSaltService = new HashSaltService();
-            bool login = hashSaltService.VerifyPassword(passwordTextBox.Text, user.HashSalt);
+            try
+            {
+
+                if (username == String.Empty)
+                    throw new Exception("No username entered");
+
+                if (password == String.Empty)
+                    throw new Exception("No password entered");
+
+                // get an user by the given username
+                UserService userService = new UserService();
+                User user = userService.GetUser(username);
+
+                if (user == null)
+                    throw new Exception("Not an existing user");
+
+                // verify the login
+                HashSaltService hashSaltService = new HashSaltService();
+                bool login = hashSaltService.VerifyPassword(passwordTextBox.Text, user.HashSalt);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                LoggerService.WriteLog(exception);
+            }
+
+            
 
             /*if (login)
             {
@@ -52,15 +76,20 @@ namespace SomerenUI
             //MessageBox.Show(login.ToString());
 
 
+            LoadSomerenUI();
+
+
+
+
+
+        }
+
+        void LoadSomerenUI()
+        {
             this.Hide();
             SomerenUI somerenUI = new SomerenUI();
             somerenUI.ShowDialog();
             this.Close();
-
-
-
-
-
         }
     }
 }
